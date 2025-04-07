eaafbbe74feb1b386a118070187df2c24d6d0355
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-04-07 16:33:54 +0500

    initialized main function, created instances of the app and JSONRPCserver, added bodyParser for express to read body of the json request

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+const express = require ('express');
+const { JSONRPCServer } = require('json-rpc-2.0');
+const { bodyParse } = require('body-parse');
+
 async function main() {
+    const app = express();
+    const server = new JSONRPCServer();
+    
+    app.use(bodyParse.json());
 }
