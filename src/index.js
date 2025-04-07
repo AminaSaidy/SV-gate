bb4cfbf85569e012e915216e647dbc840076b2f6
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-04-07 17:07:46 +0500

    initialized port to 3000, created function to handle POST requests at the /rpc endpoint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 const express = require ('express');
 const { JSONRPCServer } = require('json-rpc-2.0');
-const { bodyParse } = require('body-parse');
+const { bodyParser } = require('body-parser');
 
 async function main() {
     const app = express();
     const server = new JSONRPCServer();
     
-    app.use(bodyParse.json());
+    app.use(bodyParser.json());
+
+    app.post('/rpc' , (req, res) => {
+        server.receive(req.body).then(jsonRPCResponse => {
+            if(jsonRPCResponse) {
+                res.json(jsonRPCResponse);
+            } else {
+                res.sendStatus(204);
+            }
+        });
+    });
+
+    const port = 3000;
+    app.listen(port, () => console.log(`JSON-RPC server running on port ${port}`));
 }
+
+main();
