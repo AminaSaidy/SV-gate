9f619ff3372713f50c6db2a31ae00aa5bedecf68
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-04-14 16:27:13 +0500

    wrote service for cards.new.verify

---
+++ src/modules/cards/newVerify/service.js
@@ -0,0 +1,49 @@
+const { getErrorMessage } = require("../../../error-handler/errorCodes");
+const { crypto } = require("crypto");
+
+function verifyOtpAndGenerateToken(params) {
+  const { id, code } = params.otp;
+
+  const card = mockCards.get(id);
+  if (!card) {
+    getErrorMessage(-294);
+  }
+
+  if (mockCards.otp !== code) {
+    getErrorMessage(-269);
+  }
+
+  const token = crypto.randomBytes(16).toString("hex").toUpperCase();
+  card.token = token;
+
+  return {
+    id: token,
+    username: card.username,
+    pan: maskPan(card.pan),
+    status: card.status,
+    phone: maskPhone(card.phone),
+    fullName: card.fullName,
+    balance: card.balance,
+    sms: card.sms,
+    pincnt: card.pincnt,
+    aacct: maskAacct(card.aacct),
+    par: card.par,
+    cardtype: card.cardtype,
+    holdAmount: card.holdAmount,
+    cashbackAmount: card.cashbackAmount,
+  };
+}
+
+function maskPan(pan) {
+    return pan.slice(0, 4) + "************";
+}
+
+function maskPhone(phone) {
+    return phone.slice(0, 3) + "*********";
+}
+
+function maskAacct(aacct) {
+    return aacct.slice(0, 5) + "" + aacct.slice(-5);
+}
+
+module.exports = { verifyOtpAndGenerateToken };
