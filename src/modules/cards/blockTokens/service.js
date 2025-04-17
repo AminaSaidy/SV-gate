fa7e1749eea9ab5098034d3196bff3f02e85963d
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-04-17 17:42:24 +0500

    implemented logic for blocking a token in service

--- src/modules/cards/blockTokens/service.js
+++ src/modules/cards/blockTokens/service.js
@@ -1,4 +1,10 @@
 const blockTokens = (tokens) => {
-    return tokens.map ((id, time) ={
+    return tokens.map ((id, time) => {
+        const blockUntil = Date.now() + time * 60 * 100 + 1000;
+        blockedTokens.set(id, blockUntil);
+
+        return new BlockTokensResponseDto(id, 0, 'Token successfully blocked!');
     });
 }
+
+module.exports = { blockTokens };
