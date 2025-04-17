6fbad1d2f9c15ab393d429680c978b82a14ec5b1
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-04-17 16:03:18 +0500

    finished dto

---
+++ src/modules/cards/blockTokens/dto.js
@@ -0,0 +1,14 @@
+class BlockTokensDto {
+    constructor (id, time) {
+        this.id = id;
+        this.time = time;
+    }
+}
+
+class BlockTokensResponseDto {
+    constructor (id, appName, registrationDate) {
+        this.id = id;
+        this.appName = appName;
+        this.registrationDate = registrationDate;
+    }
+}
