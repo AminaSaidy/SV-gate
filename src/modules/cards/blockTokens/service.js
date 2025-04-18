b8d9d98ed366df6321b28d1a39c38e5ba479dfb9
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-04-18 12:19:29 +0500

    added a comment for a future fix

--- src/modules/cards/blockTokens/service.js
+++ src/modules/cards/blockTokens/service.js
@@ -1,6 +1,7 @@
 const { blockedTokens } = require("../../../common/mockData");
 const { BlockTokensResponseDto } = require("./dto");
 
+//LOGIC FOR CODE 0 IS NEEDED
 const blockTokens = (tokens) => {
     return tokens.map (({ id, time }) => {
         const blockUntil = Date.now() + time * 60 * 100 + 1000;
