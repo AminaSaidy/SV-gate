0985691c6a09014ef8c6a4f16a865edb43ca3df0
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-04-23 17:01:04 +0500

    exported all the modules into index and connected them to server

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ const cardsGetTokens = require("./modules/cards/getTokens/module.js");
 const cardsRemoveToken = require("./modules/cards/removeToken/module.js");
 const cardsBlockTokens = require("./modules/cards/blockTokens/module.js");
 const cardsUnblockTokens = require("./modules/cards/unblockTokens/module.js");
-const cardsGetController = require("./modules/cards/get/module.js");
+const cardsGet = require("./modules/cards/get/module.js");
+const cardsGetExceed = require("./modules/cards/getExceed/module.js");
+const cardStatusChange = require("./modules/cards/statusChange/module.js");
+const cardPinResetOtp = require("./modules/cards/pinResetOtp/module.js");
+const cardPinResetVerify = require("./modules/cards/pinResetVerify/module.js");
+const cardsListToken = require("./modules/cards/listToken/module.js");
+const cardCheckPinfl = require("./modules/cards/checkPinfl/module.js");
+const balanceGet = require("./modules/cards/balanceGet/module.js");
+const cardsGetSdkid = require("./modules/cards/getSdkid/module.js");
+const cardsSetTranscheme = require("./modules/cards/setTranscheme/module.js");
 
 async function main() {
   const app = express();
@@ -18,8 +27,17 @@ async function main() {
   cardsRemoveToken(server);
   cardsBlockTokens(server);
   cardsUnblockTokens(server);
-  cardsGetController(server);
-  
+  cardsGet(server);
+  cardsGetExceed(server);
+  cardStatusChange(server);
+  cardPinResetOtp(server);
+  cardPinResetVerify(server);
+  cardsListToken(server);
+  cardCheckPinfl(server);
+  balanceGet(server);
+  cardsGetSdkid(server);
+  cardsSetTranscheme(server);
+
   app.use(express.json());
 
   app.post("/rpc", (req, res) => {
