6e3de9d00ca66a92b48a193907829af07496a847
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-04-23 16:27:05 +0500

    tested with postman and made some fixes

--- src/modules/cards/get/service.js
+++ src/modules/cards/get/service.js
@@ -1,16 +1,16 @@
 const { getErrorMessage } = require("../../../error-handler/errorCodes");
-const { cards } = require("../../../common/mockData");
-const { CardResponseDto } = require("./dto");
+const { mockCards } = require("../../../common/mockData");
+const { CardInfoResponseDto } = require("./dto");
 
 const getCards = (ids) => {
   return ids.map((token) => {
-    const card = cards.find((card) => card.id === token);
+    const card = mockCards.find((card) => card.id === token);
 
     if (!card) {
       throw getErrorMessage(-257); // Token Not Found
     }
 
-    return new CardResponseDto(card);
+    return new CardInfoResponseDto(card);
   });
 };
 
